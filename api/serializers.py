58f961e73e999b94459a45164b5299eb0a17ebda
Rishabh Tripathi <44160208+rt134@users.noreply.github.com>
2020-11-21 18:31:56 +0530

    Update serializers.py

--- api/serializers.py
+++ api/serializers.py
@@ -8,9 +8,9 @@ class Car(object):
 
 class CarSerializer(serializers.Serializer):
     email_id            = serializers.CharField(max_length=30)
+    car_name            = serializers.CharField(max_length=30)
     car_number          = serializers.CharField(max_length=30)
     car_model           = serializers.CharField(max_length=30)
-    car_name            = serializers.CharField(max_length=30)
     co_ordinates        = serializers.CharField(max_length=30)
     cost_perday         = serializers.IntegerField()
     earnings            = serializers.IntegerField()
@@ -77,3 +77,93 @@ class UserSerializer(serializers.Serializer):
         for field, value in validated_data.items():
             setattr(instance, field, value)
         return instance
+
+
+
+# -------------------------------------------------------------VERIFICATION-----------------------------------------------------------------------#
+class Verify(object):
+    def __init__(self, **kwargs):
+        for field in ('sr', 'checked_email', 'employee_email','type_of'):
+            setattr(self, field, kwargs.get(field, None))
+
+
+class VerifySerializer(serializers.Serializer):
+    sr                  = serializers.IntegerField()
+    checked_email       = serializers.CharField(max_length=30)
+    employee_email      = serializers.CharField(max_length=30)
+    type_of             = serializers.CharField(max_length=30)
+    
+    def create(self, validated_data):
+        return Verify(**validated_data)
+
+    def update(self, instance, validated_data):
+        for field, value in validated_data.items():
+            setattr(instance, field, value)
+        return instance
+
+# ------------------------------------------------------EMPLOYEE------------------------------------------------------------------------------#
+class Employee(object):
+    def __init__(self, **kwargs):
+        for field in ('email_id', 'contact', 'earnings','verified_count'):
+            setattr(self, field, kwargs.get(field, None))
+
+
+class EmployeeSerializer(serializers.Serializer):
+    email_id            = serializers.CharField(max_length=30)
+    contact             = serializers.CharField(max_length=30)
+    earnings            = serializers.IntegerField() 
+    verified_count      = serializers.IntegerField()
+
+    def create(self, validated_data):
+        return Employee(**validated_data)
+
+    def update(self, instance, validated_data):
+        for field, value in validated_data.items():
+            setattr(instance, field, value)
+        return instance
+
+#------------------------------------------------------------cBOOKINGS--------------------------------------------------------------------------#
+class cBooking(object):
+    def __init__(self, **kwargs):
+        for field in ('sr', 'buyer_email', 'car_name','car_number','owner_email','status'):
+            setattr(self, field, kwargs.get(field, None))
+
+
+class cBookingSerializer(serializers.Serializer):
+    sr                  = serializers.IntegerField()
+    buyer_email         = serializers.CharField(max_length=30)
+    car_name            = serializers.CharField(max_length=30)
+    car_number          = serializers.CharField(max_length=30)
+    owner_email         = serializers.CharField(max_length=30)
+    status              = serializers.CharField(max_length=30)
+    
+    def create(self, validated_data):
+        return cBooking(**validated_data)
+
+    def update(self, instance, validated_data):
+        for field, value in validated_data.items():
+            setattr(instance, field, value)
+        return instance
+
+#------------------------------------------------------------mBOOKINGS--------------------------------------------------------------------------#
+class mBooking(object):
+    def __init__(self, **kwargs):
+        for field in ('sr', 'buyer_email','owner_email','status'):
+            setattr(self, field, kwargs.get(field, None))
+
+
+class mBookingSerializer(serializers.Serializer):
+    sr                  = serializers.IntegerField()
+    buyer_email         = serializers.CharField(max_length=30)
+    owner_email         = serializers.CharField(max_length=30)
+    status              = serializers.CharField(max_length=30)
+    
+    def create(self, validated_data):
+        return mBooking(**validated_data)
+
+    def update(self, instance, validated_data):
+        for field, value in validated_data.items():
+            setattr(instance, field, value)
+        return instance
+
+# -------------------------------------------------------------------------------------------------------------------------------------------------#
