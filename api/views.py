0922d987369bbd7714973a6ca887955f569d6d2f
Rishabh Tripathi <44160208+rt134@users.noreply.github.com>
2020-12-12 10:58:01 +0530

    Update views.py

--- api/views.py
+++ api/views.py
@@ -60,6 +60,27 @@ class carGDU(APIView):
         carobj = self.get_object(pk)
         serobj = CarSerializer(carobj, many=True)
         return Response(serobj.data)
+    
+    def put(self,request):
+        serializer = serializers.CarSerializer(data=request.data)
+        if serializer.is_valid():
+            task = serializer.save()
+            car_table.put_item(
+                Item={
+                    'email_id': task.email_id,
+                    'car_name': task.car_name,
+                    'car_model': task.car_model,
+                    'car_number': task.car_number,
+                    'co_ordinates': task.co_ordinates,
+                    'cost_perday': task.cost_perday,
+                    'earnings': task.earnings,
+                    'rating': task.rating,
+                    'is_available': task.is_available,
+                    'is_verified': task.is_verified,
+                }
+            )
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self,request,pk):
         car_table.delete_item(key={'email_id':pk},)
@@ -106,6 +127,25 @@ class mechGDU(APIView):
         serializer = MechanicSerializer(obj,many=True)
         return Response(serializer.data)
     
+    def post(self,request):
+        serializer = serializers.MechanicSerializer(data=request.data)
+        if serializer.is_valid():
+            task = serializer.save()
+            mechanic_table.put_item(
+                Item={
+                    'email_id': task.email_id,
+                    'g_name' : task.g_name,
+                    'co_ordinates': task.co_ordinates,
+                    'earnings': task.earnings,
+                    'rating': task.rating,
+                    'service_charge' : task.service_charge,
+                    'is_available': task.is_available,
+                    'is_verified': task.is_verified,
+                }
+            )
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
     def delete(self,request,pk):
         mechanic_table.delete_item(key={'email_id':pk},)
         return Response(status=status.HTTP_204_NO_CONTENT)
@@ -151,6 +191,26 @@ class userGDU(APIView):
         serializer = UserSerializer(obj,many=True)
         return Response(serializer.data)
     
+    def put(self,request,pk):
+        serializer = serializers.UserSerializer(data=request.data)
+        if serializer.is_valid():
+            task = serializer.save()
+            user_table.put_item(
+                Item={
+                    'email_id': task.email_id,
+                    'fname': task.fname,
+                    'lname': task.lname,
+                    'username': task.username,
+                    'age' : task.age,
+                    'gender': task.gender,
+                    'contact': task.contact,
+                    'dLicense' : task.dLicense,
+                }
+            )
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    
     def delete(self,request,pk):
         user_table.delete_item(key={'email_id':pk},)
         return Response(status=status.HTTP_204_NO_CONTENT)
@@ -191,6 +251,21 @@ class employeeGDU(APIView):
         obj = self.get_object(pk)
         serializer = EmployeeSerializer(obj,many=True)
         return Response(serializer.data)
+    
+    def put(self,request):
+        serializer = serializers.EmployeeSerializer(data=request.data)
+        if serializer.is_valid():
+            task = serializer.save()
+            emp_table.put_item(
+                Item={
+                    'email_id': task.email_id,
+                    'contact': task.contact,
+                    'earnings': task.earnings,
+                    'verified_count' : task.verified_count
+                }
+            )
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self,request,pk):
         emp_table.delete_item(key={'email_id':pk},)
@@ -211,7 +286,7 @@ class cBookingGP(APIView):
             task = serializer.save()
             cB_table.put_item(
                 Item={
-                    'sr' : str(task.sr),
+                    'sr' : task.sr,
                     'buyer_email': task.buyer_email,
                     'car_name': task.car_name,
                     'car_number': task.car_number,
@@ -234,6 +309,23 @@ class cBookingGDU(APIView):
         obj = self.get_object(pk)
         serializer = cBookingSerializer(obj,many=True)
         return Response(serializer.data)
+    
+    def put(self,request):
+        serializer = serializers.cBookingSerializer(data=request.data)
+        if serializer.is_valid():
+            task = serializer.save()
+            cB_table.put_item(
+                Item={
+                    'sr' : task.sr,
+                    'buyer_email': task.buyer_email,
+                    'car_name': task.car_name,
+                    'car_number': task.car_number,
+                    'owner_email' : task.owner_email,
+                    'status' : task.status
+                }
+            )
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 # ----------------------------------------------------mBooking CRUD---------------------------------------------------------------- #
 
@@ -271,6 +363,21 @@ class mBookingGDU(APIView):
         obj = self.get_object(pk)
         serializer = mBookingSerializer(obj,many=True)
         return Response(serializer.data)
+    
+    def put(self,request):
+        serializer = serializers.mBookingSerializer(data=request.data)
+        if serializer.is_valid():
+            task = serializer.save()
+            mB_table.put_item(
+                Item={
+                    'sr' : str(task.sr),
+                    'buyer_email': task.buyer_email,
+                    'mech_email' : task.mech_email,
+                    'status' : task.status
+                }
+            )
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 # ----------------------------------------------------verifying CRUD---------------------------------------------------------------- #
 
@@ -308,6 +415,21 @@ class verGDU(APIView):
         obj = self.get_object(pk)
         serializer = VerifySerializer(obj,many=True)
         return Response(serializer.data)
+
+    def put(self,request):
+        serializer = serializers.VerifySerializer(data=request.data)
+        if serializer.is_valid():
+            task = serializer.save()
+            ver_table.put_item(
+                Item={
+                    'sr' : task.sr,
+                    'checked_email': task.checked_email,
+                    'employee_email' : task.employee_email,
+                    'type_of' : task.type_of
+                }
+            )
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self,request,pk):
         ver_table.delete_item(key={'sr':pk},)
